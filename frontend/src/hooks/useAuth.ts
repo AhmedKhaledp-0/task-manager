a814f78269b03bb620732f7aa35a44371bda1024
AhmedKhaledp-0 <brokeninfp@gmail.com>
2025-03-24 01:38:20 +0200

    handing deployment errors

--- frontend/src/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-export const Auth = "auth";
-
-const useAuth = (options = {}) => {
-  useQuery({
-    queryKey: [Auth],
-    queryFn: getUser);
-};
-export default useAuth;
