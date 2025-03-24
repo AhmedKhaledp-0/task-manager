0080517ad81be0fbdd3b2651e44a864cc03d942b
AhmedKhaledp-0 <brokeninfp@gmail.com>
2025-03-24 19:07:05 +0200

    adding the meta tags to the index.html

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,14 +7,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./config/queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { HelmetProvider } from "react-helmet-async";
-import SEO from "./components/SEO.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <HelmetProvider>
-          <SEO />
+          {/* <SEO /> */}
           <App />
         </HelmetProvider>
         <ReactQueryDevtools initialIsOpen={false} />
