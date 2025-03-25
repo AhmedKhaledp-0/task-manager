eb6733c212e9268644a71d9e4f76a3682d7868af
AhmedKhaledp-0 <brokeninfp@gmail.com>
2025-03-25 22:42:16 +0200

    handling fil status

--- frontend/src/config/apiClient.ts
+++ frontend/src/config/apiClient.ts
@@ -12,7 +12,17 @@ const options = {
 const Api = axios.create(options);
 
 Api.interceptors.response.use(
-  (response) => response.data,
+  (response) => {
+    if (response.data && response.data.status === "fail") {
+      return Promise.reject({
+        message: response.data.message || "Operation failed",
+        status: response.status,
+        ...response.data,
+      });
+    }
+    return response.data;
+  },
+
   (error) => {
     const { status, data } = error.response;
     return Promise.reject({ status, message: data.message });
