86b322d8d6d1f2036fd632eaf570f8237ec42e9b
AhmedKhaledp-0 <brokeninfp@gmail.com>
2025-04-03 09:59:51 +0200

    feat: add dotenv package and types for environment variable management

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -34,13 +38,14 @@ export default defineConfig({
         ],
       },
       manifest: {
+        id: "/", // Specify App ID
         name: "ToTasky",
         short_name: "ToTasky",
         description: "A task management application",
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "https://task-managerdepi.vercel.app",
+        start_url: `${process.env.VITE_BASE_URL || "/"}`,
         dir: "ltr",
         scope: "/",
         display_override: ["standalone", "window-controls-overlay"],
@@ -51,97 +56,152 @@ export default defineConfig({
           {
             name: "Dashboard",
             short_name: "Dashboard",
-            url: "https://task-managerdepi.vercel.app/",
+            url: `${process.env.VITE_BASE_URL || ""}/`,
             description: "View your dashboard",
+            icons: [
+              {
+                src: `${process.env.VITE_BASE_URL || ""}/dashboard.png`,
+                sizes: "96x96",
+                type: "image/png",
+              },
+            ],
           },
           {
             name: "Projects",
             short_name: "Projects",
-            url: "https://task-managerdepi.vercel.app/projects",
+            url: `${process.env.VITE_BASE_URL || ""}/projects`,
             description: "View your projects",
+            icons: [
+              {
+                src: `${process.env.VITE_BASE_URL || ""}/projects.png`,
+                sizes: "96x96",
+                type: "image/png",
+              },
+            ],
           },
           {
             name: "Tasks",
             short_name: "Tasks",
-            url: "https://task-managerdepi.vercel.app/tasks",
+            url: `${process.env.VITE_BASE_URL || ""}/tasks`,
             description: "View your tasks",
+            icons: [
+              {
+                src: `${process.env.VITE_BASE_URL || ""}/tasks.png`,
+                sizes: "96x96",
+                type: "image/png",
+              },
+            ],
           },
           {
             name: "Reports",
             short_name: "Reports",
-            url: "https://task-managerdepi.vercel.app/reports",
+            url: `${process.env.VITE_BASE_URL || ""}/reports`,
             description: "View your reports",
+            icons: [
+              {
+                src: `${process.env.VITE_BASE_URL || ""}/reports.png`,
+                sizes: "96x96",
+                type: "image/png",
+              },
+            ],
           },
         ],
         screenshots: [
-          // {
-          //   src: "/screenshots/screen-1-1440x900.png",
-          //   sizes: "1440x900",
-          //   type: "image/png",
-          //   label: "ToTasky Dark View",
-          // },
-          // {
-          //   src: "/screenshots/screen-2-1440x900.png",
-          //   sizes: "1440x900",
-          //   type: "image/png",
-          //   label: "ToTasky Light View",
-          // },
-          // {
-          //   src: "/screenshots/screen-3-1440x900.png",
-          //   sizes: "1440x900",
-          //   type: "image/png",
-          //   label: "ToTasky Project Details View",
-          // },
-          // {
-          //   src: "/screenshots/screen-4-1440x900.png",
-          //   sizes: "1440x900",
-          //   type: "image/png",
-          //   label: "ToTasky Report View",
-          // },
           {
-            src: "/screenshots/screen-1-414x896.png",
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-1-1440x900.png`,
+            sizes: "1440x900",
+            type: "image/png",
+            label: "ToTasky Dark View",
+            form_factor: "wide",
+            platform: "web",
+          },
+          {
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-2-1440x900.png`,
+            sizes: "1440x900",
+            type: "image/png",
+            label: "ToTasky Light View",
+            form_factor: "wide",
+            platform: "web",
+          },
+          {
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-3-1440x900.png`,
+            sizes: "1440x900",
+            type: "image/png",
+            label: "ToTasky Project Details View",
+            form_factor: "wide",
+            platform: "web",
+          },
+          {
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-4-1440x900.png`,
+            sizes: "1440x900",
+            type: "image/png",
+            label: "ToTasky Report View",
+            form_factor: "wide",
+            platform: "web",
+          },
+          {
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-1-414x896.png`,
             sizes: "414x896",
             type: "image/png",
             label: "ToTasky Dark Mobile View",
           },
           {
-            src: "/screenshots/screen-2-414x896.png",
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-2-414x896.png`,
             sizes: "414x896",
             type: "image/png",
             label: "ToTasky Light Mobile View",
+            platform: "android ios",
           },
           {
-            src: "/screenshots/screen-3-414x896.png",
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-3-414x896.png`,
             sizes: "414x896",
             type: "image/png",
             label: "ToTasky Project Details Mobile View",
+            platform: "android ios",
           },
           {
-            src: "/screenshots/screen-4-414x896.png",
+            src: `${
+              process.env.VITE_BASE_URL || ""
+            }/screenshots/screen-4-414x896.png`,
             sizes: "414x896",
             type: "image/png",
             label: "ToTasky Report Mobile View",
+            platform: "android ios",
           },
         ],
         icons: [
           {
-            src: "/pwa-64x64.png",
+            src: `${process.env.VITE_BASE_URL || ""}/pwa-64x64.png`,
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: "/pwa-192x192.png",
+            src: `${process.env.VITE_BASE_URL || ""}/pwa-192x192.png`,
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: `${process.env.VITE_BASE_URL || ""}/pwa-512x512.png`,
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "/maskable-icon-512x512.png",
+            src: `${process.env.VITE_BASE_URL || ""}/maskable-icon-512x512.png`,
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
